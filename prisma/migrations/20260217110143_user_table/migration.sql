8d4151ad32f22ad247b59a694b6769cdf6f16a39
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-02-17 19:10:13 +0800

    feat: initialize Prisma schema with User model and indexes

--- prisma/migrations/20260217110143_user_table/migration.sql
+++ prisma/migrations/20260217110143_user_table/migration.sql
@@ -4,21 +4,41 @@ CREATE TYPE "Role" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'EMPLOYEE');
 -- CreateEnum
 CREATE TYPE "Status" AS ENUM ('PENDING', 'ACTIVE', 'INACTIVE');
 
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('LOCAL', 'GOOGLE');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" BIGSERIAL NOT NULL,
+    "employeeId" TEXT NOT NULL,
     "firstname" TEXT NOT NULL,
     "lastname" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "role" "Role" NOT NULL,
     "status" "Status" NOT NULL DEFAULT 'PENDING',
-    "passwordHash" TEXT NOT NULL,
+    "passwordHash" TEXT,
     "dataPolicyAccepted" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "provider" "Provider" NOT NULL DEFAULT 'LOCAL',
+    "isApproved" BOOLEAN NOT NULL DEFAULT false,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "verificationToken" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_employeeId_key" ON "User"("employeeId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_role_status_idx" ON "User"("role", "status");
+
+-- CreateIndex
+CREATE INDEX "User_isApproved_status_idx" ON "User"("isApproved", "status");
+
+-- CreateIndex
+CREATE INDEX "User_provider_email_idx" ON "User"("provider", "email");
