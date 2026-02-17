880b2028314e284c3a301f966e5664ed6977d54b
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-02-17 19:59:39 +0800

    added a centralized dependency injection prisma service to avoid numerous instancing

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,50 @@
+import env from 'dotenv';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private node_env: string;
+
+  constructor() {
+    const environment = process.env.NODE_ENV || 'development';
+    const path = `.env.${environment}`;
+
+    env.config({ path: path });
+
+    const { NODE_ENV } = process.env;
+
+    const connectionString = `${process.env.DATABASE_URL}`;
+    const adapter = new PrismaPg({ connectionString });
+
+    super({ adapter });
+
+    console.log('prismaservice at:', NODE_ENV);
+  }
+
+  //the connect, disconnect and trnsacrion belongs
+  //to parent PrismaClient since PrismaService inherits
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      console.log('db connected');
+    } catch (e) {
+      console.log('error at:', e.message);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('db disconnected');
+  }
+
+  async executeTransaction<T>(
+    fn: (prisma: PrismaClient) => Promise<T>,
+  ): Promise<T> {
+    return this.$transaction(fn);
+  }
+}
