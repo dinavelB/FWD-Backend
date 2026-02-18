049d7bd71fb2e4b22778813cd7320eb1857a6240
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-02-18 19:24:00 +0800

    added role default to employee

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,7 @@
-import { Role } from '@prisma/client';
-
 export class CreateUserDto {
   firstname: string;
   lastname: string;
   employeeId: string;
   email: string;
   passwordHash: string;
-  role: Role;
 }
